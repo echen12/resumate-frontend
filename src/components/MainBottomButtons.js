144be44523a4cb26273f04bcd3076400a16af039
Vivian Chu <mchu65@my.bcit.ca>
2023-03-11 14:20:07 -0800

    Modify button css

--- src/components/MainBottomButtons.js
+++ src/components/MainBottomButtons.js
@@ -5,10 +5,12 @@ import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 
 function MainBottomButtons() {
+    const divStyle = {
+        background: '#D9E2FC'};
     return (
         <Stack gap={2} className="col-md-5 mx-auto">
-            <Button variant="outline-secondary btn-primary" style={{ color:"#FFFFFF"}}>Upload Resume</Button>
-            <Button variant="outline-secondary btn-primary" style={{ color: "#FFFFFF" }}>Analyze</Button>
+            <Button variant="outline-primary btn-primary" style={divStyle}>Upload Resume</Button>
+            <Button variant="outline-primary  btn-primary" style={divStyle}>Analyze</Button>
         </Stack>
     );
 }
