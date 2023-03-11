8e63991080eed9a22281d4e98dbf56bd46c94c65
Anna Yujeong An <yujeong99an@gmail.com>
2023-03-11 15:40:25 -0800

    fix merge conflict

--- src/App.js
+++ src/App.js
@@ -4,13 +4,56 @@ import NavBar from './components/NavBar';
 import TextBox from './components/TextBox'
 import MainBottomButtons from './components/MainBottomButtons';
 
+import axios from 'axios';
+import { useState, useEffect } from 'react'
 
-const App = () => (
-  <Container>
-    <NavBar></NavBar>
-    <Logo></Logo>
-    <MainBottomButtons></MainBottomButtons>
-  </Container>
-)
+const App = () => {
+  const [ courseData ] = useState(null)
+
+  function getCourseData() {  
+    axios({
+      method: "GET",
+      url: "http://localhost:5000"
+    })
+    .then(response => {
+      console.log(response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+
+  function postResumePdf() {
+      axios({
+        // mode: 'no-cors',
+        method: 'POST',
+        url: 'http://localhost:5000/temp/',
+        data: {
+            title: 'Fred',
+            lastName: 'Flintstone',
+        },
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+          // 'Accept'       : 'application/json',
+          // 'Content-Type' : 'application/json',
+          // "Access-Control-Allow-Origin": "*",
+          // "Access-Control-Allow-Methods": "*",
+          // "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+        },
+    }).then(function (response) {
+        console.log(response);
+    }).catch(function (error) {
+        console.log(error);
+    });
+  }
+
+  return (
+    <Container>
+      <NavBar></NavBar>
+      <Logo></Logo>
+      <MainBottomButtons onClick={postResumePdf()}></MainBottomButtons>
+    </Container>
+  )
+}
 
 export default App;
