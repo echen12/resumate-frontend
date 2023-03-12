056db5adba226c45377188b6bb23632f4bc5394d
Vivian Chu <mchu65@my.bcit.ca>
2023-03-11 23:57:27 -0800

    Add footer.

---
+++ src/pages/Footer.js
@@ -0,0 +1,15 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Footer() {
+  return (
+    <Container fluid="md" 
+      <Row>
+        &copy; 2023 BCIT Hack the Break
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer
