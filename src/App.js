cb2f8619aa1a731acad3d2cf32493b4b79210906
echen12 <chen.edmond13@gmail.com>
2023-03-11 16:21:42 -0800

    potential post fix

--- src/App.js
+++ src/App.js
@@ -8,44 +8,76 @@ import axios from 'axios';
 import { useState, useEffect } from 'react'
 
 const App = () => {
-  const [ courseData ] = useState(null)
+  const [courseData] = useState(null)
 
-  function getCourseData() {  
+  const form = new FormData();
+
+  const filePath = '/resources/Resume.pdf';
+
+  form.append("files", filePath)
+
+
+  function getCourseData() {
     axios({
       method: "GET",
       url: "http://localhost:5000"
     })
-    .then(response => {
-      console.log(response.data)
-    }).catch(error => {
-      console.log(error)
-    })
+      .then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
   }
 
-  function postResumePdf() {
-      axios({
-        // mode: 'no-cors',
-        method: 'POST',
-        url: 'http://localhost:5000/temp/',
-        data: {
-            title: 'Fred',
-            lastName: 'Flintstone',
-        },
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-          // 'Accept'       : 'application/json',
-          // 'Content-Type' : 'application/json',
-          // "Access-Control-Allow-Origin": "*",
-          // "Access-Control-Allow-Methods": "*",
-          // "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-        },
-    }).then(function (response) {
-        console.log(response);
-    }).catch(function (error) {
-        console.log(error);
-    });
-  }
+  axios({
+    // mode: 'no-cors',
+    method: 'POST',
+    url: 'http://127.0.0.1:5000/temp',
+    data: {
+      id: 11,
+      name: "Tom Brady",
+      username: "Brad",
+      email: "tombrad@asd.com",
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+      // 'Accept'       : 'application/json',
+      // 'Content-Type' : 'application/json',
+      // "Access-Control-Allow-Origin": "*",
+      // "Access-Control-Allow-Methods": "*",
+      // "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    },
+  }).then(function (response) {
+    console.log(response);
+  }).catch(function (error) {
+    console.log(error);
+  });
+
+  // function postResumePdf() {
+  //   axios({
+  //     // mode: 'no-cors',
+  //     method: 'POST',
+  //     url: 'http://localhost:5000/temp/',
+  //     data: {
+  //       title: 'Fred',
+  //       lastName: 'Flintstone',
+  //     },
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+  //       // 'Accept'       : 'application/json',
+  //       // 'Content-Type' : 'application/json',
+  //       // "Access-Control-Allow-Origin": "*",
+  //       // "Access-Control-Allow-Methods": "*",
+  //       // "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+  //     },
+  //   }).then(function (response) {
+  //     console.log(response);
+  //   }).catch(function (error) {
+  //     console.log(error);
+  //   });
+  // }
 
   return (
     <Container>
@@ -55,7 +87,7 @@ const App = () => {
       <br></br>
       <br></br>
       <TextBox></TextBox>
-      <MainBottomButtons onClick={postResumePdf()}></MainBottomButtons>
+      <MainBottomButtons></MainBottomButtons>
     </Container>
   )
 }
