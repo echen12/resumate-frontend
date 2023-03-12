5aab64f192ed163db4e99f622e96f63e5c6a5ab4
Anna Yujeong An <yujeong99an@gmail.com>
2023-03-12 06:47:51 -0700

    Merge MainComponents translation update

--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -6,81 +6,78 @@ import Stack from 'react-bootstrap/Stack';
 import React, { Component, useState } from "react";
 //import { useState, useEffect } from 'react'
 import axios from 'axios';
-import {useTranslation} from "react-i18next";
+import i18next, { t } from "i18next";
 import { Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
-class MainComponents extends Component {
+
+const MainComponents = () => {
     //[ state ] = useState("");
-    button_style = {
+    const [jobDescription, setJobDescription] = useState("")
+    const [resume, setResume] = useState("")
+    const {t, i18n} = useTranslation('common');
+
+
+    const buttonStyle = {
         background: '#8f8ab3',
         color: '#ffffff',
         width: '100%'
-    };
+    }
 
-    state = {
-        jobDescription: "",
-        resume: ""
-    };
+    // button_style = {
 
-    // onSubmit = () => {
-    //     axios({
-    //         method: 'POST',
-    //         url: 'http://127.0.0.1:5000/job_resume/',
-    //         data: this.state,
-    //         headers: {
-    //           "Access-Control-Allow-Origin": "*",
-    //           "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-    //         },
-    //       }).then(response => {
-    //         console.log(response.data);
-    //       }).catch(error => {
-    //         console.log(error);
-    //       });
     // };
 
-    render() {
-        return (
-            <>
-                <Form>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>Job Description</Form.Label>
-                        <Form.Control 
-                            as="textarea" 
-                            rows={9} 
-                            placeholder="Paste your job description here" 
-                            onChange={e => this.setState({ jobDescription: e.target.value })}
-                            value={this.state.jobDescription}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>Resume</Form.Label>
-                        <Form.Control
-                            as="textarea"
-                            rows={9}
-                            placeholder="Paste your resume here"
-                            onChange={e => this.setState({ resume: e.target.value })}
-                            value={this.state.resume}
-                            />
-                    </Form.Group>
-                </Form>
-                <Stack gap={2} className="col-md-5 mx-auto" >
-                    {/* <Button variant="outline-light btn-dark" style={divStyle}>Upload Resume</Button> */}
-                    <Link to="/options" state={this.state}>
-                        <Button 
-                            variant="outline-light  btn-dark" 
-                            style={this.button_style}
-                            onClick={this.onSubmit}>
-                                Analyze
-                        </Button>
-                    </Link>
-                    <div className="bg-light "></div>
-                    <div className="bg-light "></div>
-                    <div className="bg-light "></div>
-                </Stack>
-            </>
-        );
-    }
-    
+    //  {t, i18n} = useTranslation('common');
+
+
+    // state = {
+    //     jobDescription: "",
+    //     resume: ""
+    // };
+
+    return (
+        <>
+            <Form>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>{t("Job description")}</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={9}
+                        placeholder={t("Paste your job description here")}
+                        onChange={e => setJobDescription(e.target.value)}
+                        value={jobDescription}
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>{t("Resume")}</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={9}
+                        placeholder={t("Paste your resume here")}
+                        onChange={e => setResume(e.target.value)}
+                        value={resume}
+                    />
+                </Form.Group>
+            </Form>
+            <Stack gap={2} className="col-md-5 mx-auto" >
+                {/* <Button variant="outline-light btn-dark" style={divStyle}>Upload Resume</Button> */}
+                <Link to="/options" state={{jobDescription, resume}}>
+                    <Button
+                        variant="outline-light  btn-dark"
+                        style={buttonStyle}
+                    >
+                        {t("Analyze")}
+                    </Button>
+                </Link>
+                <div className="bg-light "></div>
+                <div className="bg-light "></div>
+                <div className="bg-light "></div>
+            </Stack>
+        </>
+    );
+
+
 }
 
 export default MainComponents
